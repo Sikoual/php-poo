b71d9a1137f455376ac1596bdb29c47327552d02
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 21:02:56 +0200

    Challenge 3 cs

--- MotorWay.php
+++ MotorWay.php
@@ -1,17 +1,17 @@
 <?php
-    
-    final class MotorWay extends Highway
-    {
-        public function __construct(
-            protected array $currentVehicles = [],
-            protected int $nbLane = 4,
-            protected int $maxSpeed = 130
-        ) {
-            parent::__construct($currentVehicles, $nbLane, $maxSpeed);
-        }
 
-        public function addVehicle($vehicle): void
-        {
-            $this->currentVehicles[] = $vehicle;
-        }
+final class MotorWay extends Highway
+{
+    public function __construct(
+        protected array $currentVehicles = [],
+        protected int $nbLane = 4,
+        protected int $maxSpeed = 130
+    ) {
+        parent::__construct($currentVehicles, $nbLane, $maxSpeed);
     }
+
+    public function addVehicle($vehicle): void
+    {
+        $this->currentVehicles[] = $vehicle;
+    }
+}
--- PedestrianWay.php
+++ PedestrianWay.php
@@ -1,19 +1,19 @@
 <?php
-    
-    final class PedestrianWay extends Highway
-    {
-        public function __construct(
-            protected array $currentVehicles = [],
-            protected int $nbLane = 1,
-            protected int $maxSpeed = 10
-        ) {
-            parent::__construct($currentVehicles, $nbLane, $maxSpeed);
-        }
 
-        public function addVehicle($vehicle)
-        {
-            if ($vehicle instanceof Bicycle || $vehicle instanceof Skateboard) {
-                $this->currentVehicles[] = $vehicle;
-            }
+final class PedestrianWay extends Highway
+{
+    public function __construct(
+        protected array $currentVehicles = [],
+        protected int $nbLane = 1,
+        protected int $maxSpeed = 10
+    ) {
+        parent::__construct($currentVehicles, $nbLane, $maxSpeed);
+    }
+
+    public function addVehicle($vehicle)
+    {
+        if ($vehicle instanceof Bicycle || $vehicle instanceof Skateboard) {
+            $this->currentVehicles[] = $vehicle;
         }
     }
+}
