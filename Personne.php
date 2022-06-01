fbf4c4cd1edc4064c19740f6726ea5fe6a9c6553
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 20:39:47 +0200

    Challange7 cs

--- Personne.php
+++ Personne.php
@@ -1,65 +1,63 @@
 <?php
-    
-    class Personne
-    {
-        public function __construct(
-            public string $nom,
-            public string $prenom,
-            public string $adresse,
-            public string $date,
-        ) {
-        }
-        
-        public function getLastName(): string
-        {
-            return $this->nom;
-        }
-        
-        public function setLastName(string $lastName): void
-        {
-            $this->nom = $lastName;
-        }
-        
-        public function getFirstName(): string
-        {
-            return $this->prenom;
-        }
-        
-        public function setFirstName(string $firstName): string
-        {
-            $this->prenom = $firstName;
-        }
-        
-        public function getAdress(): string
-        {
-            return $this->adresse;
-        }
-        
-        public function setAdress(string $adress): void
-        {
-            $this->adresse = $adress;
-        }
-        
-        public function getBirthday(): string
-        {
-            return $this->date;
-        }
-        
-        public function setBirthday(string $birthday): void
-        {
-            $this->date = $birthday;
-        }
-        
-        public function afficherInformations(): string
-        {
-            return "La personne s'appelle $this->prenom $this->nom née le $this->date elle habite au $this->adresse";
-        }
-        
-        public function ageNow(): string
-        {
-            $now = time();
-            $timestamp = strtotime($this->date);
-            $age = round(($now - $timestamp) / 60 / 60 / 24 / 365, 0);
-            return "Tony Stark aurait $age ans aujourd'hui. ";
-        }
+
+class Personne
+{
+    public function __construct(
+        public string $lastName,
+        public string $firstName,
+        public string $adress,
+        public DateTime $date,
+    ) {
     }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): string
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getAdress(): string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): void
+    {
+        $this->adress = $adress;
+    }
+
+    public function birthDate(): string
+    {
+        return $this->date->format('d/m/Y');
+    }
+
+    public function displayInformation(): string
+    {
+        return 'La personne se nomme'." ".$this->firstName." "
+            .$this->lastName." ".'elle est née le'." ".$this->date->format(
+                "d/m/y"
+            )." ".'et habite au'." ".$this->adress;
+    }
+
+    public function ageNow(): string
+    {
+        $now = new DateTime('now');
+        $age = $now->diff($this->date);
+
+        return $age->format("%Y ans");
+    }
+}
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-    
-    require_once 'Personne.php';
-    
-    $ironman = new Personne('Stark', 'Tony', 'Manoir Stark', "23/05/1970");
-    
-    echo $ironman->afficherInformations();
-    echo '<br>';
-    echo $ironman->ageNow();
+
+require_once 'Personne.php';
+
+$ironman = new Personne(
+    'Stark',
+    'Tony',
+    'Manoir Stark', new DateTime('1970-05-23')
+);
+
+echo $ironman->displayInformation();
+echo $ironman->ageNow();
