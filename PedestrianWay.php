c5e6e32cbb996678e89c8545f9fb7d6c9b1a19a1
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 20:59:10 +0200

    Challenge3 cs

--- PedestrianWay.php
+++ PedestrianWay.php
@@ -1,21 +1,21 @@
 <?php
-    
-    final class PedestrianWay extends Highway
+
+final class PedestrianWay extends Highway
+{
+    public function __construct(
+        protected array $currentVehicles = [],
+        protected int $nbLane = 1,
+        protected int $maxSpeed = 10
+    ) {
+        parent::__construct($currentVehicles, $nbLane, $maxSpeed);
+        $this->nbLane = $nbLane;
+        $this->maxSpeed = $maxSpeed;
+    }
+
+    public function addVehicle($vehicle)
     {
-        public function __construct(
-            protected array $currentVehicles = [],
-            protected int $nbLane = 1,
-            protected int $maxSpeed = 10
-        ) {
-            parent::__construct($currentVehicles, $nbLane, $maxSpeed);
-            $this->nbLane = $nbLane;
-            $this->maxSpeed = $maxSpeed;
-        }
-        
-        public function addVehicle($vehicle)
-        {
-            if ($vehicle instanceof Bicycle || $vehicle instanceof Skateboard) {
-                $this->currentVehicles[] = $vehicle;
-            }
+        if ($vehicle instanceof Bicycle || $vehicle instanceof Skateboard) {
+            $this->currentVehicles[] = $vehicle;
         }
     }
+}
--- ResidentialWay.php
+++ ResidentialWay.php
@@ -1,19 +1,19 @@
 <?php
-    
-    final class ResidentialWay extends Highway
-    {
-        public function __construct(
-            protected array $currentVehicles = [],
-            protected int $nbLane = 2,
-            protected int $maxSpeed = 50
-        ) {
-            parent::__construct($currentVehicles, $nbLane, $maxSpeed);
-            $this->nbLane = $nbLane;
-            $this->maxSpeed = $maxSpeed;
-        }
-        
-        public function addVehicle($vehicle)
-        {
-            $this->currentVehicles[] = $vehicle;
-        }
+
+final class ResidentialWay extends Highway
+{
+    public function __construct(
+        protected array $currentVehicles = [],
+        protected int $nbLane = 2,
+        protected int $maxSpeed = 50
+    ) {
+        parent::__construct($currentVehicles, $nbLane, $maxSpeed);
+        $this->nbLane = $nbLane;
+        $this->maxSpeed = $maxSpeed;
     }
+
+    public function addVehicle($vehicle)
+    {
+        $this->currentVehicles[] = $vehicle;
+    }
+}
