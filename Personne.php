f5e2bdaa8a492697b7ee239ff7ac2344dda3ec50
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 20:36:53 +0200

    Update Personne.php

--- Personne.php
+++ Personne.php
@@ -1,65 +1,63 @@
 <?php
-    
+
 class Personne
 {
     public function __construct(
-        public string $nom,
-        public string $prenom,
-        public string $adresse,
-        public string $date,
+        public string $lastName,
+        public string $firstName,
+        public string $adress,
+        public DateTime $date,
     ) {
     }
 
     public function getLastName(): string
     {
-        return $this->nom;
+        return $this->lastName;
     }
 
     public function setLastName(string $lastName): void
     {
-        $this->nom = $lastName;
+        $this->lastName = $lastName;
     }
 
     public function getFirstName(): string
     {
-        return $this->prenom;
+        return $this->firstName;
     }
 
     public function setFirstName(string $firstName): string
     {
-        $this->prenom = $firstName;
+        $this->firstName = $firstName;
     }
 
     public function getAdress(): string
     {
-        return $this->adresse;
+        return $this->adress;
     }
 
     public function setAdress(string $adress): void
     {
-        $this->adresse = $adress;
+        $this->adress = $adress;
     }
 
-    public function getBirthday(): string
+    public function birthDate(): string
     {
-        return $this->date;
+        return $this->date->format('d/m/Y');
     }
 
-    public function setBirthday(string $birthday): void
+    public function displayInformation(): string
     {
-        $this->date = $birthday;
-    }
-
-    public function afficherInformations(): string
-    {
-        return "La personne s'appelle $this->prenom $this->nom née le $this->date elle habite au $this->adresse";
+        return 'La personne se nomme'." ".$this->firstName." "
+            .$this->lastName." ".'elle est née le'." ".$this->date->format(
+                "d/m/y"
+            )." ".'et habite au'." ".$this->adress;
     }
 
     public function ageNow(): string
     {
-        $now = time();
-        $timestamp = strtotime($this->date);
-        $age = round(($now - $timestamp) / 60 / 60 / 24 / 365, 0);
-        return "Tony Stark aurait $age ans aujourd'hui. ";
+        $now = new DateTime('now');
+        $age = $now->diff($this->date);
+
+        return $age->format("%Y ans");
     }
 }
