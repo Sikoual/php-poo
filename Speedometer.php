d87df65e1d4fb3374b29aca22cb278956f2019ec
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 20:40:21 +0200

    Challenge6 cs

--- Speedometer.php
+++ Speedometer.php
@@ -1,27 +1,27 @@
 <?php
-    
-    class Speedometer
+
+class Speedometer
+{
+    public const KM100 = 100;
+    public const MILES100 = 100;
+
+    public static function convertKmToMiles($km): float
     {
-        public const KM100 = 100;
-        public const MILES100 = 100;
-        
-        public static function convertKmToMiles($km): float
-        {
-            return $km / 1.609;
-        }
-        
-        public static function convertMilesToKm($miles): float
-        {
-            return $miles * 1.609;
-        }
-        
-        public static function convert100kmToMiles(): float
-        {
-            return self::KM100 / 1.609;
-        }
-        
-        public static function convert100milesToKm(): float
-        {
-            return self::MILES100 * 1.609;
-        }
+        return $km / 1.609;
     }
+
+    public static function convertMilesToKm($miles): float
+    {
+        return $miles * 1.609;
+    }
+
+    public static function convert100kmToMiles(): float
+    {
+        return self::KM100 / 1.609;
+    }
+
+    public static function convert100milesToKm(): float
+    {
+        return self::MILES100 * 1.609;
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-    
-    require_once 'Speedometer.php';
-    
-    echo Speedometer::convertKmToMiles(10);
-    echo '<br>';
-    echo Speedometer::convertMilesToKm(10);
-    echo '<br>';
-    echo Speedometer::convert100kmToMiles();
-    echo '<br>';
-    echo Speedometer::convert100milesToKm();
+
+require_once 'Speedometer.php';
+
+echo Speedometer::convertKmToMiles(10);
+echo '<br>';
+echo Speedometer::convertMilesToKm(10);
+echo '<br>';
+echo Speedometer::convert100kmToMiles();
+echo '<br>';
+echo Speedometer::convert100milesToKm();
