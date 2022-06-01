1c0e1b2a343e98226a2d87239e633c94274e73c7
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 20:54:21 +0200

    Challenge4 cs

--- Voiture.php
+++ Voiture.php
@@ -1,62 +1,64 @@
 <?php
-    
-    class Voiture extends Vehicle
-    {
-        public const ALLOWED_ENERGIES = [
-            'fuel',
-            'electric',
-        ];
-        private int $energyLevel = 50;
-        
-        public function __construct(
-            protected string $color,
-            protected int $nbSeats,
-            protected string $energy,
-            private bool $hasParkBrake = false,
-        ) {
-            parent::__construct($color, $nbSeats);
-            $this->setEnergy($energy);
-        }
-        
-        public function start(): string
-        {
-            if ($this->hasParkBrake) {
-                throw new Error("Enlevez le frein à main !");
-            }
-            
-            if ($this->energyLevel > 0) {
-                $sentence = "Lets GOO!";
-            } else {
-                $sentence = "Oops need fuel";
-            }
-            return $sentence;
-        }
-        
-        public function getEnergy(): string
-        {
-            return $this->energy;
-        }
-        
-        public function setEnergy(string $energy): Voiture
-        {
-            if (in_array($energy, self::ALLOWED_ENERGIES)) {
-                $this->energy = $energy;
-            }
-            return $this;
-        }
-        
-        public function getEnergyLevel(): int
-        {
-            return $this->energyLevel;
-        }
-        
-        public function setEnergyLevel(int $energyLevel): void
-        {
-            $this->energyLevel = $energyLevel;
-        }
-        
-        public function setParkBrake(bool $hasParkBrake): void
-        {
-            $this->hasParkBrake = $hasParkBrake;
-        }
+
+class Voiture extends Vehicle
+{
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+    private int $energyLevel = 50;
+
+    public function __construct(
+        protected string $color,
+        protected int $nbSeats,
+        protected string $energy,
+        private bool $hasParkBrake = false,
+    ) {
+        parent::__construct($color, $nbSeats);
+        $this->setEnergy($energy);
     }
+
+    public function start(): string
+    {
+        if ($this->hasParkBrake) {
+            throw new Error("Enlevez le frein à main !");
+        }
+
+        if ($this->energyLevel > 0) {
+            $sentence = "Lets GOO!";
+        } else {
+            $sentence = "Oops need fuel";
+        }
+
+        return $sentence;
+    }
+
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+    public function setEnergy(string $energy): Voiture
+    {
+        if (in_array($energy, self::ALLOWED_ENERGIES)) {
+            $this->energy = $energy;
+        }
+
+        return $this;
+    }
+
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
+
+    public function setParkBrake(bool $hasParkBrake): void
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+}
--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
+
+require_once 'Vehicle.php';
+require_once 'Bicycle.php';
+require_once 'Voiture.php';
+require_once 'Camion.php';
+require_once 'Highway.php';
+require_once 'MotorWay.php';
+require_once 'Skateboard.php';
+require_once 'ResidentialWay.php';
+require_once 'PedestrianWay.php';
+
+$car = new Voiture('red', 4, "fuel");
+$car->setParkBrake(true);
     
-    require_once 'Vehicle.php';
-    require_once 'Bicycle.php';
-    require_once 'Voiture.php';
-    require_once 'Camion.php';
-    require_once 'Highway.php';
-    require_once 'MotorWay.php';
-    require_once 'Skateboard.php';
-    require_once 'ResidentialWay.php';
-    require_once 'PedestrianWay.php';
-    
-    $car = new Voiture('red', 4, "fuel");
-    $car->setParkBrake(true);
-    
-    try {
-        $car->start();
-    } catch (Error $e) {
-        $car->setParkBrake(false);
-    } finally {
-        echo 'Ma voiture roule comme un donut';
-    }
-    
-    var_dump($car);
+try {
+    $car->start();
+} catch (Error $e) {
+    $car->setParkBrake(false);
+} finally {
+    echo 'Ma voiture roule comme un donut';
+}
+
+var_dump($car);
