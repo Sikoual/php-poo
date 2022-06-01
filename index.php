3f72d756ba857e1901516207631774fd22d12cb6
Maxime Hu <96578270+Sikoual@users.noreply.github.com>
2022-06-01 19:27:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
     
-    require_once 'Personne.php';
-    
-    $ironman = new Personne('Stark', 'Tony', 'Manoir Stark', "23/05/1970");
-    
-    echo $ironman->afficherInformations();
-    echo '<br>';
-    echo $ironman->ageNow();
+require_once 'Personne.php';
+
+$ironman = new Personne('Stark', 'Tony', 'Manoir Stark', "23/05/1970");
+
+echo $ironman->afficherInformations();
+echo '<br>';
+echo $ironman->ageNow();
